6fd2ca32ba6d8f7d8aaa4cfd6f6fab7c8af51beb
Ahmed Hamam <ahmedelsayedhamam1@gmail.com>
2023-10-01 22:19:13 +0300

    Initial Commit

---
+++ NeflixClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NeflixClone
+//
+//  Created by Ahmed Hamam on 01/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
