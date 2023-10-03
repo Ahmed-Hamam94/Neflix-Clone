25d6c0a328d6c3a79f8f803998cfe3bb433bc0cb
Ahmed Hamam <ahmedelsayedhamam1@gmail.com>
2023-10-04 02:18:56 +0300

    add error enum

--- NeflixClone/Managers/ApiError.swift
+++ NeflixClone/Managers/ApiError.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+enum APIError: String, Error {
+    case internalError = "internalError"
+    case serverError = "serverError"
+    case parsingError = "parsingError"
+    case urlBadFormmated = "urlBadFormmated"
+    case unknownError = "unknownError"
+    case errorDecoding = "errorDecoding"
+}
