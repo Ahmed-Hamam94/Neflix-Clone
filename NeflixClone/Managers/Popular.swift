b146169ed5237eb8c06acad9899b0e32fe61afc9
Ahmed Hamam <ahmedelsayedhamam1@gmail.com>
2023-10-04 02:24:33 +0300

    add model

--- NeflixClone/Managers/Popular.swift
+++ NeflixClone/Managers/Popular.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+protocol PopularProtocol {
+    func getPopularMovies(completion: @escaping (Result<[Movie], APIError>)-> Void)
+}
+
+class PopularMovies: PopularProtocol {
+    
+    func getPopularMovies(completion: @escaping (Result<[Movie], APIError>) -> Void) {
+        NetworkManager.shared.request(endPoint: EndPoint.popular, method: .Get, completion: completion)
+    }
+    
+    
+}
