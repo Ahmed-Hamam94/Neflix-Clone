667cb648f8feb1a197707ea7b359335c347ae9db
Ahmed Hamam <ahmedelsayedhamam1@gmail.com>
2023-10-04 02:25:12 +0300

    add model

--- NeflixClone/Managers/TrendingTV.swift
+++ NeflixClone/Managers/TrendingTV.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+
+protocol TrendingTvProtocol {
+    func getTrendingTV(completion: @escaping (Result<[TV], APIError>)-> Void)
+}
+
+class TrendingTV: TrendingTvProtocol{
+    
+    func getTrendingTV(completion: @escaping (Result<[TV], APIError>) -> Void) {
+        NetworkManager.shared.request(endPoint: EndPoint.trendingTV, method: .Get, completion: completion)
+    }
+    
+    
+}
