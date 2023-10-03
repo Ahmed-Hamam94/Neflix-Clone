f31c12299d96c7897c4001676f6092e76e8d7761
Ahmed Hamam <ahmedelsayedhamam1@gmail.com>
2023-10-04 02:25:25 +0300

    add model

--- NeflixClone/Managers/UpComingMovies.swift
+++ NeflixClone/Managers/UpComingMovies.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+protocol upComingProtocol{
+    func getUpComingMovies(completion: @escaping (Result<[ComingMovies], APIError>)->Void)
+}
+
+class upComingMovies: upComingProtocol {
+    
+    func getUpComingMovies(completion: @escaping (Result<[ComingMovies], APIError>) -> Void) {
+        NetworkManager.shared.request(endPoint: EndPoint.upComing, method: .Get, completion: completion)
+    }
+ 
+
+}
