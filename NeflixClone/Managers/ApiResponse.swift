d1760c0f3892889a34c3102f316946b4a97385f7
Ahmed Hamam <ahmedelsayedhamam1@gmail.com>
2023-10-04 02:22:06 +0300

    add generic response model

--- NeflixClone/Managers/ApiResponse.swift
+++ NeflixClone/Managers/ApiResponse.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+
+struct ApiResponse<T: Codable>: Codable {
+    let dates: [String:String]? 
+    let page: Int?
+    let results: T?  
+    let totalPages: Int?
+    let totalResults: Int?
+}
