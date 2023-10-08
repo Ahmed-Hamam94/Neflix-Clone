5657f096ed15cc2730c3d7d60c4a594fa399485b
Ahmed Hamam <ahmedelsayedhamam1@gmail.com>
2023-10-08 03:50:39 +0300

    add strin extention

--- NeflixClone/Extension/String+Extension.swift
+++ NeflixClone/Extension/String+Extension.swift
@@ -5,7 +5,7 @@
 //  Created by Ahmed Hamam on 03/10/2023.
 //
 
-import Foundation
+import UIKit
 
 extension String {
     var asUrl: URL?{
@@ -15,4 +15,18 @@ extension String {
     func capitalizedFirstLetter()-> String{
         return self.prefix(1).uppercased() + self.lowercased().dropFirst()
     }
+}
+
+extension UIViewController {
+  func screen() -> UIScreen? {
+    var parent = self.parent
+    var lastParent = parent
+    
+    while parent != nil {
+      lastParent = parent
+      parent = parent!.parent
+    }
+    
+    return lastParent?.view.window?.windowScene?.screen
+  }
 }
