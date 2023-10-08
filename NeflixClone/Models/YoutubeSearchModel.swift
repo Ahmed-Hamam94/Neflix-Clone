cbd2974224f1e01263b85fbab51806ae57323b24
Ahmed Hamam <ahmedelsayedhamam1@gmail.com>
2023-10-08 03:55:24 +0300

    add model

--- NeflixClone/Models/YoutubeSearchModel.swift
+++ NeflixClone/Models/YoutubeSearchModel.swift
@@ -6,3 +6,45 @@
 //
 
 import Foundation
+
+struct YoutubeSearchResults: Codable {
+   // let items: [videoElement]
+       let kind: String
+        let etag: String
+        let nextPageToken: String
+        let regionCode: String
+       // let pageInfo: PageInfo
+        let items: [VideoElement]
+
+        private enum CodingKeys: String, CodingKey {
+            case kind = "kind"
+            case etag = "etag"
+            case nextPageToken = "nextPageToken"
+            case regionCode = "regionCode"
+           // case pageInfo = "pageInfo"
+            case items = "items"
+        }
+}
+
+struct VideoElement: Codable {
+  //  let id: IdVideoElement
+    
+        let id: IdVideoElement
+
+        private enum CodingKeys: String, CodingKey {
+         
+            case id = "id"
+        }
+}
+
+struct IdVideoElement: Codable {
+
+    let kind: String
+    let videoId: String
+
+    private enum CodingKeys: String, CodingKey {
+        case kind = "kind"
+        case videoId = "videoId"
+    }
+
+}
