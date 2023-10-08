f4831fa35a1ce680cd479fa9219dac3cd59c5436
Ahmed Hamam <ahmedelsayedhamam1@gmail.com>
2023-10-08 03:49:39 +0300

    core data class for save fetch and delete

--- NeflixClone/CoreData/DataPersistenceManager.swift
+++ NeflixClone/CoreData/DataPersistenceManager.swift
@@ -6,3 +6,68 @@
 //
 
 import Foundation
+import UIKit
+import CoreData
+
+class DataPersistenceManager {
+    
+    static let shared = DataPersistenceManager()
+        
+    private let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    private let context: NSManagedObjectContext?
+    
+    private init() {
+        context = appDelegate.persistentContainer.viewContext
+    }
+
+    func downloadMovieWith(model: Movie, completion: @escaping ( (Result<Void, Error>)-> Void) ){
+        
+        guard let context else {return}
+        let item = NetflixItem(context: context)
+        
+        item.id = Int64(model.id ?? 0)
+        item.originalTitle = model.originalTitle
+        item.originalName = model.originalName
+        item.title = model.title
+        item.overview = model.overview
+        item.posterPath = model.posterPath
+        item.mediaType = model.mediaType
+        item.releaseDate = model.releaseDate
+        item.voteAverage = model.voteAverage ?? 0.0
+        item.voteCount = Int64(model.voteCount ?? 0)
+        
+        do {
+            try context.save()
+            completion(.success(()))
+        }catch(let error){
+            completion(.failure(error))
+        }
+    }
+    
+    func fetchingMoviesFromCoreData(completion: @escaping ( (Result<[NetflixItem], Error>)-> Void) ){
+        
+        let request: NSFetchRequest<NetflixItem>
+        request = NetflixItem.fetchRequest()
+        
+        do {
+            guard let movies = try context?.fetch(request) else {return}
+            completion(.success(movies))
+        }catch(let error){
+            completion(.failure(error))
+        }
+
+    }
+    
+    func deleteMovieWith(model: NetflixItem, completion: @escaping ( (Result<Void, Error>)-> Void) ){
+        
+        context?.delete(model)
+        
+        do {
+            try context?.save()
+            completion(.success(()))
+        }catch(let error){
+            completion(.failure(error))
+        }
+    }
+   
+}
